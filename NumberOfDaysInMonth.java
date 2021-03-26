0a463520db75d9d7bc64ee7088b88a1ba823830d
Dawana Baker <66096734+kat8404@users.noreply.github.com>
2021-03-26 05:15:32 -0500

    Update NumberOfDaysInMonth.java
    Added a comment describing the method

--- NumberOfDaysInMonth.java
+++ NumberOfDaysInMonth.java
@@ -1,24 +1,5 @@
-/* Write a method isLeapYear with a parameter of type int named year. 
+/* Calculates if the provided year is a leap year, and returns true if it is, otherwise it returns false. */
 
-The parameter needs to be greater than or equal to 1 and less than or equal to 9999.
-
-If the parameter is not in that range return false. 
-
-Otherwise, if it is in the valid range, calculate if the year is a leap year and return true if it is, otherwise return false. 
-
-A year is a leap year if it is divisible by 4 but not by 100, or it is divisible by 400.
-
-​NOTE:  The solution to the Leap Year coding exercise earlier in the course created the isLeapYear method. 
-You can use that solution if you wish. */
-
-// Write another method getDaysInMonth with two parameters month and year.  ​Both of type int.
-
-// If parameter month is < 1 or > 12 return -1. ​
-// If parameter year is < 1 or > 9999 then return -1.
-
-// This method needs to return the number of days in the month. Be careful about leap years they have 29 days in month 2 (February).
-
-// You should check if the year is a leap year using the method isLeapYear described above.*/
 
 public class NumberOfDaysInMonth {
 
