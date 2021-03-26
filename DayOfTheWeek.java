d1798392df33e0d64fbdf723f9d9e872cfac6822
Dawana Baker <66096734+kat8404@users.noreply.github.com>
2021-03-26 05:09:27 -0500

    Added Days of the Week Program
    This program includes one method, with three different variations. The last example reads user input. The main method is included.

--- DayOfTheWeek.java
+++ DayOfTheWeek.java
@@ -1,20 +1,10 @@
-/* Write a method with the name printDayOfTheWeek that has one parameter of type int and name it day.
-
-The method should not return any value (hint: void)
-
-Using a switch statement print Sunday, Monday, .... , Saturday if the int parameter "day" is 0, 1, 3, 4, 5 , 6 respectively, 
-otherwise it should print "Invalid day".
-
-Bonus: 
-	Write a second solution using if then else, instead of using switch.
-	Create a new project in IntelliJ with the  name DayOfTheWeekChallenge
- */
+ 
 
 public class DayOfTheWeek {
 
     public static void main(String[] args) {
         
-        printDayOfTheWeek(8);
+        printDayOfTheWeek(3);
     
     }
     
@@ -49,7 +39,8 @@ public class DayOfTheWeek {
     }
 
 
-    //If-else version of the above switch statement.
+
+    //<--------------If-else version of the above switch statement.------------->
 
     // if (day == 0) {
     //     System.out.println("Sunday");
@@ -71,3 +62,36 @@ public class DayOfTheWeek {
     }
 }
 
+/* 
+<--------------A version with user-input!---------------->
+
+    public static boolean DayOfTheWeek() {
+
+    Scanner scan = new Scanner(System.in);
+
+    System.out.println("Please choose between 0-6.");
+    int day = scan.nextInt();
+
+    if ((day < 0) || (day > 6)) {
+        System.out.println("Invalid Number. Ender between 0-6.");
+}   else if (day == 0) {
+        System.out.println("Sunday");
+}   else if (day == 1) {
+        System.out.println("Monday");
+}   else  if (day == 2) {
+        System.out.println("Tuesday");
+}   else if (day == 3) {
+        System.out.println("Wednesday");
+}   else if (day == 4) {
+        System.out.println("Thursday");
+}   else if (day == 5) {
+        System.out.println("Friday");
+}   else if (day == 6) {
+        System.out.println("Saturday");
+}   else {
+        System.out.println("Invalid numnber. Enter between 0-6.");
+        scan.close();
+}
+return true;
+    }
+} */
